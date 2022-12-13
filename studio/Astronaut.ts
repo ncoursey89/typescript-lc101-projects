b427d1c14dd9417bc79426c5893c901f58a6e8d4
ncoursey89 <nathanael.coursey@gmail.com>
2022-12-13 00:15:57 -0600

    TypeScriptExerciseUpload

---
+++ studio/Astronaut.ts
@@ -0,0 +1,9 @@
+import { Payload } from './Payload';
+export class Astronaut {
+class Astronaut implements Payload {
+    constructor(massKg: number, name: string;) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+}
+}
